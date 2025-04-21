a696db12f6a07137d860970e2fa72bbb085c1ffa
bielma <thetonybl@gmail.com>
2025-04-21 08:10:10 -0600

    Add Answer model, controller, and migration for storing answers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AnswerController;
 use App\Http\Controllers\MenuController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuestionController;
@@ -9,6 +10,11 @@ Route::get('/', function () {
 });
 
 Route::prefix('api')->group(function () {
-    Route::resource('questions', QuestionController::class);
+    Route::resource('questions', QuestionController::class);    
     Route::resource('menu_completo', MenuController::class);
+    Route::resource('answers', AnswerController::class);    
+    Route::get('get_csrf', function () {
+        return response()->json(['csrf_token' => csrf_token()]);
+    });
 });
+
