3e3a852141dd5b368883fe82d07d034fd7f198c7
bielma <thetonybl@gmail.com>
2025-04-22 02:45:27 -0600

    Add foreign key constraint for user_id in answers table migration

--- database/migrations/2025_04_18_170024_create_answers_table.php
+++ database/migrations/2025_04_18_170024_create_answers_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('answers', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->foreignId('question_id')->constrained('questions')->onDelete('cascade'); 
             $table->string('answer');            
             $table->timestamps();
